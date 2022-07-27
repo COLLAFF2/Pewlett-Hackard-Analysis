5bd541a106ca254c9eaa7b21203bd51a1f8aac98
Phillip Rodriguez <cmpet28@yahoo.com>
2022-07-27 16:25:44 -0400

    new tables and query

--- queries.sql
+++ queries.sql
@@ -38,3 +38,105 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+ select * from current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+--creating table for employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+Into emp_count
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+Select * From salaries
+Order By to_date DESC;
+
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	      AND (de.to_date = '9999-01-01');
+
+--List of managers per department
+Select dm.dept_no,
+       d.dept_name,
+	   dm.emp_no,
+	   ce.last_name,
+	   ce.first_name,
+	   dm.from_date,
+	   dm.to_date
+Into manager_info
+From dept_manager As dm
+    Inner Join departments as d
+	    ON (dm.dept_no= d.dept_no)
+	Inner join current_emp As ce
+	    On ( dm.emp_no = ce.emp_no);
+
+Select *from manager_info;
+
+Select ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+ Into dept_info
+From current_emp as ce
+inner Join dept_employees As de
+On(ce.emp_no = de.emp_no)
+Inner Join departments As d
+On(de.dept_no = d.dept_no);
+       
